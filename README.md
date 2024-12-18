eca01dabae46965765bcf93dbd5d6b31bad8dc5c
xstabux <buksta@outlook.com>
2024-12-18 04:38:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Building for Desktop Testing
+
+`gradlew jar`
+
+Mod jar will appear in the `build/libs` directory. **Only use this version for testing on desktop. It will not work with Android.**
+
+## Running Mindustry directly
+
+`gradlew runClient`
+
+This Mindustry uses the `run` directory to store its data.
---
+++ src/ss/SSMod.kt
@@ -0,0 +1,19 @@
+package ss
+
+import arc.util.*
+import mindustry.mod.*
+
+class SSMod : Mod(){
+
+    init{
+        Log.info("Loaded Spins & Stress constructor.")
+    }
+
+    override fun init(){
+        Log.info("Loaded Spins & Stress init.")
+    }
+
+    override fun loadContent(){
+        Log.info("Loading some Spins & Stress content.")
+    }
+}
